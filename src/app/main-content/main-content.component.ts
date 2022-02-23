6f1ac0cead43f605923a65e13194ca623a85ef54
Ali-Ahmad2002 <aliahmad.557755@gmail.com>
2022-02-24 00:17:28 +0100

    foodsInBasket

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -8,24 +8,36 @@ import { ArraysService } from '../arrays.service';
 })
 export class MainContentComponent implements OnInit {
 
+  activeBtn: boolean = true;
+
   constructor(public arrays: ArraysService) { }
 
   ngOnInit(): void {
-   
+
   }
 
   showFood(category: string) {
     if (category !== 'all') {
       console.log(category);
       this.arrays.filteredFood = this.arrays.foods.filter((f: any) => f.category === category);
+      this.activeBtn = true;
     } else {
       this.arrays.filteredFood = this.arrays.foods;
+      this.activeBtn = false;
     }
   }
 
   addToBasket(food: any) {
-    this.arrays.foodsInBasket.push(food);
-    console.log('test:', this.arrays.foodsInBasket);
-  }
+    let position = this.arrays.foodsInBasket.indexOf(this.arrays.filteredFood[food]);
+    let index = this.arrays.filteredFood[food];
+    console.log('pos', position);
+    if (position == -1) {
+      index.amount = 1;
+      this.arrays.foodsInBasket.push(index);
+    } else {
+      index.amount++;
+    }
 
+
+  }
 }
--- src/app/shopping-basket/shopping-basket.component.ts
+++ src/app/shopping-basket/shopping-basket.component.ts
@@ -7,6 +7,8 @@ import { ArraysService } from '../arrays.service';
   styleUrls: ['./shopping-basket.component.scss']
 })
 export class ShoppingBasketComponent implements OnInit {
+  activeFood: boolean = false;
+  currentAmount: number = 1;
 
   currentPosition = window.pageYOffset;
 
@@ -26,14 +28,48 @@ export class ShoppingBasketComponent implements OnInit {
 
   ngOnInit(): void {
     console.log('basket', this.arrays.foodsInBasket);
+    setInterval(() => {
+      this.basketFood();
+    }, 25);
   }
 
-  onScroll(event: any) {
-    console.log(event);
-    if (this.currentPosition > 0) {
-      
+  basketFood() {
+    if (this.arrays.foodsInBasket.length > 0) {
+      this.activeFood = true;
+    } else if (this.arrays.foodsInBasket.length == 0) {
+      this.activeFood = false;
     }
+  }
 
+  onScroll(event: any, cart: any) {
+    console.log(event);
+    console.log('Y', window.scrollY);
+  }
+
+  subTotal() {
+    let subSum = this.arrays.foodsInBasket.price * this.arrays.foodsInBasket.amount;
+    console.log('sum:', subSum);
+
+  }
+
+  addAmount(a: number) {
+    this.arrays.foodsInBasket[a].amount++;
+  }
+
+
+  lowerAmount(a: number) {
+    let index = this.arrays.foodsInBasket.indexOf(a);
+    this.arrays.foodsInBasket[a].amount--;
+    console.log(this.arrays.foodsInBasket[a].amount);
+    let thePrice = +this.arrays.foodsInBasket[a].price;
+    console.log('price', thePrice);
+    let theAmount = +this.arrays.foodsInBasket[a].amount;
+    let sum = theAmount * thePrice;
+    console.log('theSum', sum);
+    if (this.arrays.foodsInBasket[a].amount < 1) {
+      this.arrays.foodsInBasket[a].amount = 1;
+      this.arrays.foodsInBasket.splice(index, 1);
+    }
   }
 
 }
